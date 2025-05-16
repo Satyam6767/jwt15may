108bd6cc1fe06b47224aba0a69adf0e74838eb49
Satyam6767 <satyamkumar6767@gmail.com>
2025-05-16 11:06:38 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,9 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
